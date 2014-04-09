863c5679a3ce8da3d49b017bfd870011114666d0
Jorge Palacios <pctroll@gmail.com>
2014-04-09 14:41:39 -0430

    Obstacle avoidance with one ray

--- Assets/AiBlender/Scripts/AiBehaviors/AiObstacleAvoidance.cs
+++ Assets/AiBlender/Scripts/AiBehaviors/AiObstacleAvoidance.cs
@@ -7,25 +7,44 @@ public class AiObstacleAvoidance : AiSeek {
 
 	public float m_LookAhead;
 
+	private int m_Layer;
 	// Use this for initialization
 	void Start () {
 		Init();
 	}
 
-	public override void Init ()
-	{
+	public override void Init () {
 		base.Init ();
+		m_Target = new GameObject();
+		m_Target.name = "AiObstacleAvoidance";
+		m_Layer = LayerMask.NameToLayer("AiWall");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
+	public override void Update () {
+		if (m_Agent != null) {
+			m_Agent.SetSteering(GetSteering());
+		}
 	}
 
 	public override AiSteering GetSteering () {
+		m_Steering = new AiSteering();
 		if (m_Agent != null) {
-
+			Vector3 position = gameObject.transform.position;
+			// error in vector with velocity, it's look ahead
+			Vector3 rayVector = GetOriAsVec(m_Agent.Orientation) * m_LookAhead;
+			rayVector += position;
+			Vector3 direction = rayVector - position;
+			RaycastHit hit;
+			if (m_DrawLines) {
+				Debug.DrawRay(gameObject.transform.position, direction, Color.red);
+			}
+			if (Physics.Raycast(position, direction, out hit, m_LookAhead)) {
+				position = hit.point + hit.normal * m_AvoidDistance;
+				m_Target.transform.position = position;
+				m_Steering = base.GetSteering();
+			}
 		}
-		return base.GetSteering ();
+		return m_Steering;
 	}
 }
